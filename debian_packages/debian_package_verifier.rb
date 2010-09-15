33db7f6094806d493c95663d10aeda3c456d8306
gamphani <dave.phiri@baobabhealth.org>
2010-09-15 10:33:04 +0200

    Improved code formating

--- debian_packages/debian_package_verifier.rb
+++ debian_packages/debian_package_verifier.rb
@@ -1,10 +1,12 @@
 class Debian_packages_verifier
-   $installed_packages=[]
-   Dir.foreach("/var/lib/dpkg/info") do |pkg|
-   $installed_packages << pkg.gsub(/.list/,"") if  pkg.to_s.include?('.list') end
+  $installed_packages=[]
+  
+  Dir.foreach("/var/lib/dpkg/info") do |pkg|
+    $installed_packages << pkg.gsub(/.list/,"") if  pkg.to_s.include?('.list') 
+  end
 
-
-  def verify_one_debian_package(installed_packages,package) #checking one debian package
+  #checking one debian package
+  def verify_one_debian_package(installed_packages,package)
     @package=package
     @installed_packages=installed_packages
     
