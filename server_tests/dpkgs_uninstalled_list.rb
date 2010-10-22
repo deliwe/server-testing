5ecb1f4249689e99e57ceb93467ac6071a680dcb
Deliwe Nkhoma <dbnkhoma@gmail.com>
2010-10-22 14:20:46 +0200

    adding modified codes

--- server_tests/dpkgs_uninstalled_list.rb
+++ server_tests/dpkgs_uninstalled_list.rb
@@ -1,14 +1,17 @@
 #!/usr/bin/ruby -w
 # listing the unistalled debian packages from dpkgs_verifier.rb
+# and message if all installed
 #
 require 'dpkgs_verifier.rb'
 
 class DpkgsUninstalledList
-  #list all uninstalled debian pkgs
-  #
   def list_uninstalled_dpkgs  
-	  puts " List of unistalled debian packages.."
-	  $uninstalled_pkgs.each{ |pkg| puts pkg }
+    if $uninstalled_pkgs.count == 0
+      puts" All important debian packages are installed"
+    else
+      puts " List of unistalled debian packages.."
+	    $uninstalled_pkgs.each{ |pkg| puts pkg }
+    end
   end
 end
 gems = DpkgsUninstalledList.new
--- server_tests/gems_uninstalled_list.rb
+++ server_tests/gems_uninstalled_list.rb
@@ -4,9 +4,13 @@
 require 'gems_verifier.rb'
 
 class GemsUninstalledList
-  def list_uninstalled_gems     
-    puts " List of unistalled gems.."
-	  $uninstalled_gems.each{ |gem| puts gem.chop }
+  def list_uninstalled_gems 
+    if $uninstalled_gems.count == 0
+      puts " All important gems are installed"
+    else   
+      puts " List of unistalled gems.."
+	    $uninstalled_gems.each{ |gem| puts gem.chop }
+	  end
   end
 end
 gems = GemsUninstalledList.new
